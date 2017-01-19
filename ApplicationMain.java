bb1513a539f815ba6bb536a29577142506cd7e69
Matt Higgins <mrhiggi@g.clemson.edu>
2017-01-19 18:16:57 -0500

    Circle and dialog box now fully implemented, need to tweak circle/window size and position

--- ApplicationMain.java
+++ ApplicationMain.java
@@ -10,15 +10,21 @@ Base file provided by Oracle Java JavaFX HelloWorld tutorial.
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.shape.Circle;
 import javafx.scene.paint.Color;
+import java.util.Random;
+import java.util.Optional;
  
 public class ApplicationMain extends Application {
     public static void main(String[] args) {
@@ -36,10 +42,25 @@ public class ApplicationMain extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+		final Random rand = new Random();
+		final Color paintArray[] = {Color.RED, Color.BLUE, Color.BLACK, Color.GREEN, 
+				Color.DARKGOLDENROD}; 
         primaryStage.setTitle("Hello World!");
 		final Scene scene = new Scene(new VBox(), 300, 250);
 
 		final Circle circle = new Circle();
+		circle.setOnMouseClicked(new EventHandler<MouseEvent>(){
+			public void handle(MouseEvent t){
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Information Dialog");
+				alert.setHeaderText(null);
+				alert.setContentText("You have successfully selected the circle!");
+
+				Optional<ButtonType> result = alert.showAndWait();
+				if (result.isPresent() && result.get() == ButtonType.OK)
+					System.exit(0);
+			}
+		});
 		MenuBar menuBar = new MenuBar();
 
 		Menu menu = new Menu("File");
@@ -56,8 +77,9 @@ public class ApplicationMain extends Application {
 					circle.setCenterX(100.0f);
 					circle.setCenterY(100.0f);
 					circle.setRadius(50.0f);
-					circle.setFill(Color.RED);
+					circle.setFill(paintArray[rand.nextInt(paintArray.length)]);
 					((VBox) scene.getRoot()).getChildren().addAll(circle);
+					ApplicationMain.setCircleState(1);
 				}
 			}
 		});
